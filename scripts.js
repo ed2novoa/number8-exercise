40279ece6189e9bd217a6082bce3a8fcaf3c7bcc
r2d2cr <33237517+r2d2cr@users.noreply.github.com>
2017-10-31 16:27:13 -0600

    Rendering the calendar but still without holidays

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+$( function() {
+
+} );
 
 
-console.log('hello');
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+function renderCalendar(){
+
+  var startDateString = $( "#date-input" ).val();
+  var startDateObject = new Date(startDateString);
+  //Some kind a bug got me back one day
+  startDateObject = startDateObject.addDays(1);
+  console.dir(startDateObject);
+  var numberOfDays = $( "#number-of-days-input" ).val();
+
+  var endDateObject = startDateObject.addDays(numberOfDays);
+  var endDateString = endDateObject.getFullYear() + "-" + (endDateObject.getMonth()+1) + "-" + endDateObject.getDate();
+
+  $.datepicker.setDefaults({
+          dateFormat: 'yy-mm-dd',
+          minDate: startDateString,
+          maxDate: endDateString
+    });
+
+  $( "#calendar-div" ).datepicker();
+}
